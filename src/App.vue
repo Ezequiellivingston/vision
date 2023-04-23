183c39b053a4d18550e73b3e2c724b364d30b3f5
plivingston <plivingston@sofredigital.com.ar>
2023-04-23 19:24:52 -0300

    create watchposition

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref, watch } from "vue";
 
 const text = ref("");
+const save = ref([]);
 
 let rec;
 if (!("webkitSpeechRecognition" in window)) {
@@ -31,6 +32,7 @@ function addCreateObject(obj) {
       (data.latitud = position.coords.latitude),
         (data.longitud = position.coords.longitude);
       text.value = data;
+      save.value.push(data)
     };
     navigator.geolocation.getCurrentPosition(success, function (msg) {
       console.error(msg);
@@ -38,12 +40,36 @@ function addCreateObject(obj) {
   }
 }
 
+function initSeartch(positionObj){
+  console.log(positionObj)
+  var success = function (position) {
+      console.log(position);
+    };
+  navigator.geolocation.watchPosition(success, function (msg) {
+      console.error(msg);
+    })
+}
+
+function searchObject(obj){
+  const data = save.value.find((e)=> {
+    if(e.name.includes(obj)){
+      return e
+    }
+  })
+  initSeartch(data)
+}
+
 function iniciar(event) {
   for (let i = event.resultIndex; i < event.results.length; i++) {
     text.value = event.results[i][0].transcript;
     if (event.results[i][0].transcript.includes("guardar")) {
-      if (event.results[i][0].transcript.split("guardar")[1]) {
-        addCreateObject(event.results[i][0].transcript.split("guardar")[1]);
+      if (event.results[i][0].transcript.split("guardar ")[1]) {
+        addCreateObject(event.results[i][0].transcript.split("guardar ")[1]);
+      }
+    }
+    if (event.results[i][0].transcript.includes("Buscar")) {
+      if (event.results[i][0].transcript.split("Buscar ")[1]) {
+        searchObject(event.results[i][0].transcript.split("Buscar ")[1]);
       }
     }
   }
